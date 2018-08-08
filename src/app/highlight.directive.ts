5bf28722e4b58999567c2b11d3e4bd1ad915dd7e
equiman <equiman@users.noreply.github.com>
2018-08-07 22:58:07 -0500

    change ost listener event

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
-  constructor(private elementRef: ElementRef) {
+  @HostListener('mouseenter')
+  onmouseenter() {
     this.elementRef.nativeElement.style.backgroundColor = 'yellow';
   }
+  @HostListener('mouseleave')
+  onmouseleave() {
+    this.elementRef.nativeElement.style.backgroundColor = null;
+  }
+
+  constructor(private elementRef: ElementRef) { }
 
 }
